07685ec75887b4e7db19305c0846c6e0f78d4bb0
J-P Nurmi <jp.nurmi@canonical.com>
2023-08-08 16:30:16 +0200

    fix: use GlobalUbuntuInitLocalizations.delegates

--- lib/l10n.dart
+++ lib/l10n.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:ubuntu_localizations/ubuntu_localizations.dart';
-import 'package:ubuntu_provision/l10n.dart';
+import 'package:ubuntu_init/ubuntu_init.dart';
 
 export 'package:flutter_gen/gen_l10n/app_localizations.dart';
 export 'package:ubuntu_localizations/ubuntu_localizations.dart';
@@ -9,6 +8,5 @@ export 'package:ubuntu_provision/l10n.dart';
 
 const localizationsDelegates = <LocalizationsDelegate<dynamic>>[
   ...AppLocalizations.localizationsDelegates,
-  ...UbuntuProvisionLocalizations.localizationsDelegates,
-  ...GlobalUbuntuLocalizations.delegates,
+  ...GlobalUbuntuInitLocalizations.delegates,
 ];
