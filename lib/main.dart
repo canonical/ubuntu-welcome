a8a2833a33a88bcaeb55c767d877a91d202cfc76
J-P Nurmi <jp.nurmi@canonical.com>
2023-08-11 12:55:41 +0200

    Write ~/.config/ubuntu-welcome-done (#44)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:ubuntu_init/ubuntu_init.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -9,5 +10,14 @@ Future<void> main(List<String> args) {
     package: 'ubuntu_welcome',
     localizationsDelegates: AppLocalizations.localizationsDelegates,
     onGenerateTitle: (context) => AppLocalizations.of(context).appTitle,
+    onDone: () {
+      final home =
+          Platform.environment['REALHOME'] ?? Platform.environment['HOME'];
+      try {
+        File('$home/.config/ubuntu-welcome-done').createSync();
+      } on FileSystemException catch (e) {
+        stderr.writeln(e);
+      }
+    },
   );
 }
